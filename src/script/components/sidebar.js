0557282eaa5a86b5d71a12ae533e8fa017e2628c
Kartik Sundriyal <kartik.sundriyal@joshtechnologygroup.com>
2025-06-21 13:11:48 +0530

    [KS-RU-01]: Refactored the changes needed for header and menubar | PR review fixes

--- src/script/components/sidebar.js
+++ src/script/components/sidebar.js
@@ -1,25 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
     const navMenu = document.querySelector('.header__menubar');
     const navLinks = document.querySelector('.header__nav-links');
-    const menuBtn = document.querySelector('.header__menu-logo');
-    const closeBtn = document.querySelector('.icon-cross');
+    const menuBtn = document.querySelector('.header__hamburger-icon');
+
+    if (!navMenu || !navLinks || !menuBtn) {
+        return;
+    }
 
     menuBtn.addEventListener('click', () => {
-        if (window.getComputedStyle(navMenu).position == 'absolute') {
+        if (window.getComputedStyle(navMenu).position === 'absolute') {
             navMenu.classList.toggle('header__menubar--visible');
         } else navLinks.classList.toggle('header__nav-links--visible');
+
+        menuBtn.classList.toggle('icon-cross');
     });
 
     document.addEventListener('click', (e) => {
         const clickedInsideSidebar =
             navMenu.contains(e.target) || navLinks.contains(e.target);
         const clickedMenuBtn = menuBtn.contains(e.target);
-        const clickedCloseBtn = closeBtn.contains(e.target);
 
-        if ((!clickedInsideSidebar && !clickedMenuBtn) || clickedCloseBtn) {
-            if (window.getComputedStyle(navMenu).position == 'absolute') {
+        if (!clickedInsideSidebar && !clickedMenuBtn) {
+            if (window.getComputedStyle(navMenu).position === 'absolute') {
                 navMenu.classList.remove('header__menubar--visible');
             } else navLinks.classList.remove('header__nav-links--visible');
+            menuBtn.classList.toggle('icon-cross');
         }
     });
 });
