3699d67df38c0399594ec50ae6313b1e960fc994
Kartik Sundriyal <kartik.sundriyal@joshtechnologygroup.com>
2025-07-01 17:15:28 +0530

    [KS-RU-07]: Testimonials section fixes.

--- src/script/utils/glide.js
+++ src/script/utils/glide.js
@@ -1,29 +1,13 @@
 import Glide from '@glidejs/glide';
-const glide = new Glide('.glide', {
+
+const GLIDE_CONFIG = {
     type: 'carousel',
     startAt: 0,
     perView: 1,
-    autoplay: 2500,
+    autoplay: 2000,
     animationDuration: 600,
-});
+};
 
-const prevBtn = document.querySelector('.glide__arrow--left');
-const nextBtn = document.querySelector('.glide__arrow--right');
-const totalSlides = document.querySelectorAll(
-    '.glide__slide:not(.glide__slide--clone)',
-).length;
-
-function updateArrowStates(index) {
-    prevBtn.disabled = index === 0;
-    nextBtn.disabled = index === totalSlides - 1;
-}
-
-glide.on('mount.after', () => {
-    updateArrowStates(glide.index);
-});
-
-glide.on('run.after', () => {
-    updateArrowStates(glide.index);
-});
+const glide = new Glide('.glide', GLIDE_CONFIG);
 
 glide.mount();
